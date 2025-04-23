9614b71133bd93a9fc9e002552d7d43f3feb7d8e
FedeZawa <127407817+FedeZawa@users.noreply.github.com>
2025-04-23 17:46:43 -0300

    Update header.h

--- header.h
+++ header.h
@@ -27,6 +27,12 @@ typedef struct{
     char Texto_batalla[100];
 } Villano;*/
 
+//struct de PRUEBA para crear y escribir un archivo
+typedef struct{
+    char textoNarrativo[50];
+    char batalla[30];
+} Capitulo;
+
 int menu_principal();//mostrar inicio: "iniciar juego", "salir", "continuar aventura"
 FILE* cargar_partida(int n);
 void batalla(FILE, Personaje); //se desarrolla cada batalla
@@ -34,6 +40,10 @@ int sigue_con_vida(Personaje);
 void mostrar_estado(Personaje);// mostrar vida, acciones disponibles, etc
 void guardar_proceso(FILE); //?
 
+void crear_archivo_historia();
+void crear_archivo_Personajes_Principales();
+
+void historia_principal(Personaje *Personajes);
 //todo esto lo copie de lo que mando fedezawa al discord
 
 #endif // HEADER_H_INCLUDED
