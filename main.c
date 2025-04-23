abd178d170962bdafbe04a52dab318871765142b
FedeZawa <federicozawadzki@gmail.com>
2025-04-23 17:39:51 -0300

    cambios 23/4

--- main.c
+++ main.c
@@ -3,13 +3,23 @@
 int main(){
 
     FILE* juego;
-    int selec; //
+    int selec;
+
+    //crear_archivo_historia();
+    //crear_archivo_Personajes_Principales()
+
     selec = menu_principal();//mostrar opciones en pantalla tales como cargar partida, nueva partida, salir del juego, (opciones?: posibilidad de poner juego en ingles? algo más?)
     if(selec == 3) return 0;
     juego = cargar_partida(selec);
-    
+
+    historia_principal(juego);
+
     return 0;
 }
+
+
+
+//FUNCIONES
 
 int menu_principal(){
 
@@ -53,7 +63,7 @@ FILE* cargar_partida(int n){
             puts("No se pudo cargar la partida");
             exit(1);
         }
-        
+
     }
     else{ // nueva partida
         game= fopen("Partida.dat","wb");
@@ -63,8 +73,100 @@ FILE* cargar_partida(int n){
             exit(1);
         }
     }
-    /* si se carga la partida hay que extraer la informacino y almacenarla en la memoria dinamica 
+
+    return game;
+    /* si se carga la partida hay que extraer la informacino y almacenarla en la memoria dinamica
     sino se crea el archivo y se los debe cargar con los datos iniciales de la partida*/
 }
 
 guardar_partida(FILE* game); // guardar juego, se podria preguntar si desa salir del juego o si desea seguir jugando.
+
+
+/*
+El archivo que contenga la historia principal, puede contener en cada linea un texto que narre lo que sucede/ va a suceder
+y el nombre del archivo de batalla al que se haga referencia
+*/
+//Inicio Funciones de prueba, y tener como ejemplo (al menos por ahora) para trabajar y ver como realizar la historia
+void crear_archivo_historia() {
+    FILE* archivo = fopen("Historia Principal", "wb");
+    if (archivo == NULL) {
+        puts("No se pudo crear el archivo");
+        exit(1);
+    }
+
+    Capitulo capitulos[3] = {
+        {"Ejemplo de texto para rellenar","batalla 1.bin"},
+        {"Ejemplo de texto para rellenar antes de la 2da batalla","batalla 2.bin"},
+        {"Más texto de ejemplo rellenar","batalla 3.bin"}
+    };
+    fwrite(capitulos, sizeof(Capitulo), 3, archivo);
+    fclose(archivo);
+}
+
+
+void crear_archivo_Personajes_Principales() {
+    FILE* archivo = fopen("Historia Principal", "wb");
+    if (archivo == NULL) {
+        puts("No se pudo crear el archivo");
+        exit(1);
+    }
+
+    Personaje personajes[3] = {
+        {"Personaje 1",100,30,20},
+        {"Personaje 12",100,20,60},
+        {"Personaje 3",100,10.2,70}
+    };
+    fwrite(personajes, sizeof(Personaje), 3, archivo);
+    fclose(archivo);
+}
+
+//FIN Funciones de prueba
+
+
+void historia_principal(Personaje *personajes){
+    FILE* archivo = fopen("Historia Principal", "rb");
+    if (archivo == NULL) {
+        puts("No se pudo crear el archivo");
+        exit(1);
+    }
+
+    Capitulo capituloN;
+    Personaje *auxPersonajes = personajes;
+
+    while(fread(&capituloN, sizeof(Capitulo), 1, archivo) == 1){
+        printf("\n****************************");
+        printf("\n%s",capituloN.texto_narrativo);
+        printf("\nA que personaje deseas utilizar?");
+        for(int i=0; i<3; i++){
+            printf("\n%d- %s",i+1,auxPersonajes->Nombre);
+            auxPersonajes++;
+        }
+        scanf("%d", &opcion);
+
+        int opcion=0;
+        while(opcion>3 || opcion <=0){
+            printf("Entrada no valida. Intentalo de nuevo.\n\n");
+            getchar();
+            scanf("%d", &opcion);
+        }
+
+        batalla(capituloN.batalla,personajes,opcion);
+    }
+
+    fclose(archivo);
+}
+
+//devuelve 1-> gana el héroe , 0-> gana el villano
+int batalla(const char nombreArchivo,Personaje* per,int opcion){
+    FILE* archivo = fopen(nombreArchivo, "");
+    if (archivo == NULL) {
+        puts("No se pudo crear el archivo");
+        exit(1);
+    }
+
+    //continuar
+}
+
+
+
+
