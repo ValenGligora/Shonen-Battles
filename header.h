e70be778d27e306537ad3ebe43a3770663eb4265
diegoklp1 <llanosdiegowk@gmail.com>
2025-05-04 18:31:57 -0300

    Reestrucruación proyecto: Ordené cabeceras y funciones Estructura villano eliminada: Se puede usar la de personaje Inventario de jugador: Se va usar un vector de estructuras donde cada una tenga el nombre del objeto y cantidad disponible

--- header.h
+++ header.h
@@ -1,49 +1,92 @@
 #ifndef HEADER_H_INCLUDED
 #define HEADER_H_INCLUDED
 
-#include <stdio.h>
 #include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+#include <time.h>
 
 #define NO_SE_ENCONTRO_PARTIDA 2
- 
+
+
+ // ------------------------------ ESTRUCTURAS --------------------------- //
 typedef struct{
-    char elemento[5][30];
-    int ce;
+        char elemento[30];
+        int usos;           // Cantidad de veces que el usuario puede usar el elemento
 } Inventario;
 
 typedef struct{
+    char nombre[30];
+    float danio_porcentual;
+} Arma;
+
+typedef struct
+{
+	char nombre[30];
+	float ataque_tec;
+	int cosmo_necesario;
+
+} Tecnica; // estar�a interesante hacer un tda que reserve espacio seg�n la cantidad de habilidad que tenga el personaje, sino pongo como maximo 3 tecnicas
+
+typedef struct
+{
     char Nombre[20];
     float Vida;
+    float ataque;
     float Cosmo;
     float Armadura;
-    Inventario invent; // si el invetario es una matriz se podria hacer que cada columna corresponda a un personaje por lo que esta declaracion no haria falta
+    float defensa;
+    Arma arma;
+    Tecnica tecnicas[5];
+    int cant_tec;
+    Inventario invent[3];
 } Personaje;
 
-/*typedef struct{ es necesario este struct o podemos reutilizar el de personaje?
-    char Nombre[20];
-    float Vida;
-    float Cosmo;
-    float Armadura;
-    char Texto_batalla[100];
-} Villano;*/
+//Struct de prueba Capitulo
+/*
+        struct de PRUEBA para crear y escribir un archivo
+    typedef struct{
+        char textoNarrativo[50];
+        char batalla[30];
+    } Capitulo;
+*/
 
-//struct de PRUEBA para crear y escribir un archivo
-typedef struct{
-    char textoNarrativo[50];
-    char batalla[30];
-} Capitulo;
 
-int menu_principal();//mostrar inicio: "iniciar juego", "salir", "continuar aventura"
-FILE* cargar_partida(int n);
-void batalla(FILE, Personaje); //se desarrolla cada batalla
-int sigue_con_vida(Personaje);
-void mostrar_estado(Personaje);// mostrar vida, acciones disponibles, etc
-void guardar_proceso(FILE); //?
 
-void crear_archivo_historia();
-void crear_archivo_Personajes_Principales();
+// ------------------------------ -------------------- --------------------------- //
 
-void historia_principal(Personaje *Personajes);
-//todo esto lo copie de lo que mando fedezawa al discord
+// -------------------------ENCABEZADO FUNCIONES -----------------------//
+
+    //--PRUEBA--
+        void crear_archivo_historia();
+        //void crear_archivo_Personajes_Principales();
+
+    //--ARCHIVO PARTIDA--
+        FILE* cargar_partida(int n);
+        //void guardar_proceso(FILE);                               // Puede ser una funci�n o algo autom�tico en el main (probablemente sea una funci�n)
+
+    //--JUEGO--
+        //int menu_principal();                                     // Mostrar inicio: "iniciar juego", "salir", "continuar aventura"
+        //void historia_principal(Personaje *Personajes);           // Recorrer el archivo de historia
+
+    //--BATALLA--
+        void batalla(FILE, Personaje);          // Se desarrolla cada batalla
+        int sigue_con_vida(Personaje);          // Consulta si sigue con vida
+        void mostrar_estado(Personaje);         // Mostrar vida, acciones disponibles, etc
+
+        void InicializarPersonaje(Personaje *); // el siguiente parametro seria el nombre del archivo con los atributos del personaje
+        void InicializarPersonaje2(Personaje *);
+        int TurnosBatalla(Personaje *prota,Personaje *enemigo,int *resultado);
+
+        int ValidarEleccion(int,int,int,int *,Tecnica tecnicas[],float cosmo);
+        int validarIntRango(int,int);
+        int EleccionRandomEnemigo(Personaje* enemigo,int *opt_tec_enemigo);
+
+        void EjecutarAccion(int ,int,Personaje *,Personaje *,int );
+        float CalcularAtaque(Personaje *prota);
+        float CalcularAtaqueTecnica(Personaje *p,int num_tec);
+        void recibeDanio(float danio,Personaje *p,int eleccion);
+
+// ------------------------------ -------------------- --------------------------- //
 
 #endif // HEADER_H_INCLUDED
--- main.c
+++ main.c
@@ -1,175 +1,28 @@
 #include "header.h"
+#include "funciones.c"
 
 int main(){
 
     FILE* juego;
     int selec;
 
-    //crear_archivo_historia();
-    //crear_archivo_Personajes_Principales()
+    //crear_archivo_historia();                 // Archivo de prueba de historia
+    //crear_archivo_Personajes_Principales()    // Archivo de prueba de personaje
+
+    // ----------------------INICIO DE MENU Y CARGA DE JUEGO --------------------//
 
     selec = menu_principal();//mostrar opciones en pantalla tales como cargar partida, nueva partida, salir del juego, (opciones?: posibilidad de poner juego en ingles? algo más?)
-    if(selec == 3) return 0;
+    if(selec == 3) 
+        return 0; // opcion numero 2 (Salir)
+    
     juego = cargar_partida(selec);
 
+    // -------------------------INICIO DE MENU DE JUEGO -----------------------//
+    
     historia_principal(juego);
+    
 
     return 0;
 }
-
-
-
-//FUNCIONES
-
-int menu_principal(){
-
-    int opcion=0;
-        printf("+--------------------------------+\n");
-        printf("|     SAINT SEIYA THE GAME       |\n");
-        printf("+--------------------------------+\n");
-        //printf("|  1. Continuar partida          |\n"); que cambia de cargar partida? y como se podria implementar?
-        printf("|  2. Cargar partida             |\n");
-        printf("|  3. Nueva partida              |\n");
-        printf("|  4. Salir del juego            |\n");
-        printf("+--------------------------------+\n");
-        //se podria hacer mas bonito
-    printf("\nElige una opcion (2-4): ");
-    scanf("%d", &opcion);
-
-    while (opcion<2 ||opcion>4){
-
-        printf("Entrada no valida. Intentalo de nuevo.\n\n");
-        getchar();
-        scanf("%d", &opcion);
-    }
-
-    switch (opcion) {
-        case 1: printf("\n-> Continuando la partida...\n\n"); break;
-        case 2: printf("\n-> Cargando partida...\n\n");   break;
-        case 3: printf("\n-> Iniciando nueva partida...\n\n"); break;
-        default: printf("\nSaliendo del juego. ¡Hasta la proxima!\n");
-        }
-
-    return opcion;
-}
-
-FILE* cargar_partida(int n){
-
-    FILE* game;
-    if(n==2){ // cargar partida
-        game = fopen("Partida.dat","rb");
-        if (game == NULL)
-        {
-            puts("No se pudo cargar la partida");
-            exit(1);
-        }
-
-    }
-    else{ // nueva partida
-        game= fopen("Partida.dat","wb");
-        if (game == NULL)
-        {
-            puts("No se pudo crear la partida");
-            exit(1);
-        }
-    }
-
-    return game;
-    /* si se carga la partida hay que extraer la informacino y almacenarla en la memoria dinamica
-    sino se crea el archivo y se los debe cargar con los datos iniciales de la partida*/
-}
-
-guardar_partida(FILE* game); // guardar juego, se podria preguntar si desa salir del juego o si desea seguir jugando.
-
-
-/*
-El archivo que contenga la historia principal, puede contener en cada linea un texto que narre lo que sucede/ va a suceder
-y el nombre del archivo de batalla al que se haga referencia
-*/
-//Inicio Funciones de prueba, y tener como ejemplo (al menos por ahora) para trabajar y ver como realizar la historia
-void crear_archivo_historia() {
-    FILE* archivo = fopen("Historia Principal", "wb");
-    if (archivo == NULL) {
-        puts("No se pudo crear el archivo");
-        exit(1);
-    }
-
-    Capitulo capitulos[3] = {
-        {"Ejemplo de texto para rellenar","batalla 1.bin"},
-        {"Ejemplo de texto para rellenar antes de la 2da batalla","batalla 2.bin"},
-        {"Más texto de ejemplo rellenar","batalla 3.bin"}
-    };
-    fwrite(capitulos, sizeof(Capitulo), 3, archivo);
-    fclose(archivo);
-}
-
-
-void crear_archivo_Personajes_Principales() {
-    FILE* archivo = fopen("Historia Principal", "wb");
-    if (archivo == NULL) {
-        puts("No se pudo crear el archivo");
-        exit(1);
-    }
-
-    Personaje personajes[3] = {
-        {"Personaje 1",100,30,20},
-        {"Personaje 12",100,20,60},
-        {"Personaje 3",100,10.2,70}
-    };
-    fwrite(personajes, sizeof(Personaje), 3, archivo);
-    fclose(archivo);
-}
-
-//FIN Funciones de prueba
-
-
-void historia_principal(Personaje *personajes){
-    FILE* archivo = fopen("Historia Principal", "rb");
-    if (archivo == NULL) {
-        puts("No se pudo crear el archivo");
-        exit(1);
-    }
-
-    Capitulo capituloN;
-    Personaje *auxPersonajes = personajes;
-    int opcion=0;
-
-    while(fread(&capituloN, sizeof(Capitulo), 1, archivo) == 1){
-        printf("\n****************************");
-        printf("\n%s",capituloN.texto_narrativo);
-        printf("\nA que personaje deseas utilizar?");
-        for(int i=0; i<3; i++){
-            printf("\n%d- %s",i+1,auxPersonajes->Nombre);
-            auxPersonajes++;
-        }
-        scanf("%d", &opcion);
-        auxPersonajes+-3;
-
-        while(opcion>3 || opcion <=0){
-            printf("Entrada no valida. Intentalo de nuevo.\n\n");
-            getchar();
-            scanf("%d", &opcion);
-        }
-        
-        batalla(capituloN.batalla,personajes,opcion);
-    }
-
-    fclose(archivo);
-}
-
-//devuelve 1-> gana el héroe , 0-> gana el villano
-int batalla(const char nombreArchivo,Personaje* per,int opcion){
-    FILE* archivo = fopen(nombreArchivo, "");
-    if (archivo == NULL) {
-        puts("No se pudo crear el archivo");
-        exit(1);
-    }
-
-    //continuar
-
-//prueba0 de commit diego
-}
-
-
 
 
